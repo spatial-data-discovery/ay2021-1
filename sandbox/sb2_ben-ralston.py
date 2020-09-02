2b3c6d3b05f7c308d8abe8bacc0b294fd47624e4
Ben Ralston <ben.ralston12@gmail.com>
2020-09-02 12:21:04 -0400

    Created sandbox script #2.

---
+++ sandbox/sb2_ben-ralston.py
@@ -0,0 +1,68 @@
+import os
+from GPSPhoto import gpsphoto
+from geojson import Point, Feature, FeatureCollection, dumps
+from cartopy.crs import EqualEarth, Geodetic
+import matplotlib.pyplot as plt
+
+
+def get_properties(geo_dict, file_name):
+    properties = {'image': file_name}
+    for key in geo_dict:
+        if key == 'Latitude' or key == 'Longitude':
+            continue
+        property_name = key.lower()
+        properties[property_name] = geo_dict[key]
+    return properties
+
+
+def save_geojson(image_directory, output_file):
+    image_files = os.listdir(image_directory)
+    features = []
+    for file_name in image_files:
+        file_path = os.path.join(image_directory, file_name)
+        geo_data = gpsphoto.getGPSData(file_path)
+
+        point = Point((geo_data['Latitude'], geo_data['Longitude']))
+        properties = get_properties(geo_data, file_name)
+        feature = Feature(geometry=point, properties=properties)
+        features.append(feature)
+
+    feature_collection = FeatureCollection(features)
+    with open(output_file, 'w+') as file:
+        file.write(dumps(feature_collection))
+
+    return features
+
+
+def save_plot(feature_list, pdf_path, png_path):
+    ax = plt.axes(projection=EqualEarth())
+    ax.stock_img()
+    for feature in feature_list:
+        lat, lon = feature['geometry']['coordinates']
+        plt.plot(lon, lat, 'ro', markersize=3, markeredgecolor='k',
+                 markeredgewidth=.6, transform=Geodetic())
+
+    plt.text(-86, 31, 'Images\n1 and 6',
+             horizontalalignment='right',
+             transform=Geodetic())
+    plt.text(2.5, 52, 'All other images',
+             horizontalalignment='left',
+             transform=Geodetic())
+
+    plt.savefig(pdf_path)
+    plt.savefig(png_path)
+
+
+def main():
+    photo_directory = 'photos'
+    geojson_file = 'image_location.geojson'
+    output_pdf = 'image_plot.pdf'
+    output_png = 'image_plot.png'
+
+    features = save_geojson(photo_directory, geojson_file)
+
+    save_plot(features, output_pdf, output_png)
+
+
+if __name__ == '__main__':
+    main()
