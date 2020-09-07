42dfed477ac0b7e80657706dae0ee21c94456e7d
CSStarfish <64974779+CSStarfish@users.noreply.github.com>
2020-09-07 02:43:12 -0400

    Modified my file, addresses #1

--- sandbox/sb1a_csstarfish.py
+++ sandbox/sb1a_csstarfish.py
@@ -19,4 +19,4 @@ print("Number expressed in sixtieths of a sixtieth: " + str(c))
 
 # This calculation and print statement were used to test the accuracy of the calculations for a, b, and c.
 num = a + (1/60)*b + (1/(60**2))*c
-print("\nOriginal number: " + str(user_num) + ", Calculated number: " + str(num))
+print("\nOriginal number: " + str(user_num) + ", Calculated number: " + str(num)) 
