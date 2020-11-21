c04413e8022a508b7b54dcc5f716f6af7eedc158
newfigton <ciaranlowell98@gmail.com>
2020-11-20 22:43:53 -0500

    Added my updated Conv2 code. Addresses #9 and #4.

--- conv2/conv2_newfigton.py
+++ conv2/conv2_newfigton.py
@@ -1,19 +1,13 @@
-#!/usr/bin/env python3
-#
-# nc_write.py
-#
-# LAST EDIT: 2020-03-21
-#
-# This script writes an NetCDF file.
-# Usage: run this script in tandem with nc_read.py.
-# Each time, change the next False statement to True.
-
+#Author- Ciaran Lowell
+#Last Updated- 11/20/2020
+#This script creates a netcdf function from zipped .asc raster files
 ###############################################################################
 # IMPORT MODULES
 ###############################################################################
 import os
 import os.path
 import datetime
+from datetime import date
 import numpy
 from scipy.io import netcdf
 import glob
@@ -24,94 +18,132 @@ from zipfile import ZipFile
 # GLOBAL VARIABLES:
 ##############################################################################
 ERROR_VAL = -3000
-basedate = date.fromisoformat("1900-01-01")
+basedate = date.fromisoformat('1900-01-01')
 count = 0
-convert = lambda x : (float(x)/1000)
-
 
 ###############################################################################
 # FUNCTIONS
 ###############################################################################
+
+
 def nc_history():
     """
     Name:     nc_history
     Input:    None.
     Output:   string (my_str)
-    Features: Returns a string for netCDF file history field based on the file's
-              creation date
-    """
-    my_str = "created %s" % datetime.date.today()
+    Features: Returns a file creation date string"""
+    my_str = 'created %s' % datetime.date.today()
     return my_str
 
 
+def writeFileAttributes(ncFile):
+    """
+    Name:        writeFileAttributes
+    Input:       netCDF file
+    Output:      None.
+    Features:    Writes netCDF file attributes such as author"""
+    ncFile.history = nc_history()
+    ncFile.contact = 'Ciaran Lowell (cllowell@email.wm.edu)'
+    ncFile.title = 'Monthly global EVI. 0.5 degree resolution'
+    ncFile.institution = 'William & Mary'
+    ncFile.satellite = 'Terra'
+    return
 
-###############################################################################
-# MAIN
-###############################################################################
-print("This program will convert zipped .asc files into one netCDF file.")
-print("This program must be run three times to run properly.")
-codebool = int(input('Have you run this program 0, 1, or 2 times previously?: '))
-zip_folder = input('Enter folder path to the zipped .asc files you wish to convert: ')
-zip_path = zip_folder + '\\' + "*.zip"
-files = glob.glob(zip_path)
-for zip in files:
-    with ZipFile(zip,'r') as zippy:
-        zippy.extractall(zip_folder)
-data_path = zip_folder + '\\' + "*.txt"
-datafiles = glob.glob(data_path)
-my_file = "test.nc"
-nc_path = os.path.join(zip_folder, my_file)
-# Create a new NetCDF file
-f = netcdf.netcdf_file(nc_path, 'w')
 
-if codebool == 0:
-    # Write file attributes:
-    f.history = nc_history()
-    f.contact = 'Ciaran Lowell (cllowell@email.wm.edu)'
-    f.title = 'Monthly global Enhanced Vegetation Indexes (EVI) at 0.5 degree resolution'
-    f.institution = 'William & Mary'
-    f.satellite = "Terra"
+def writeDimensions(ncFile, datafiles):
+    """
+    Name:        writeDimensions
+    Input:       netCDF file, filepath
+    Ouput:       None.
+    Features:    Creates time, lat, and long dimensions for netcdf file"""
 
-if codebool == 1:
-    # Create time dimension and a variable
-    f.createDimension('time', 12)
-    time = f.createVariable('time', 'i', ('time',) )
+    # Date info is calculated based on filename
+    # and represents days since January 1st, 1900
+    ncFile.createDimension('time', 12)
+    time = ncFile.createVariable('time', 'i', ('time', ))
     time.units = 'days since 1900-01-01'
-    for file in datafiles:
-        filedate =  date.fromisoformat(file[-14:-4])
-        print(filedate)
-        filedelta = filedate-basedate
-        print(filedelta)
-        filedays = (str(filedelta)[0:-14])
-        print(filedays)
-        time[count] = filedays
-        count +=1
-    print(time)
+    for file in os.listdir(datafiles):
+        if file.endswith('.txt'):
+            count = 0
+            print(file)
+            filedate = date.fromisoformat(file[-14:-4])
+            print(filedate)
+            filedelta = filedate-basedate
+            print(filedelta)
+            filedays = (str(filedelta)[0:-14])
+            print(filedays)
+            time[count] = filedays
+            count += 1
 
-    # Create lat dimension & variable
-    f.createDimension('latitude', 360)
-    latitude = f.createVariable('latitude', 'float', ('latitude'))
+    # Creates latitude dimension & variable
+    ncFile.createDimension('latitude', 360)
+    latitude = ncFile.createVariable('latitude', 'float', ('latitude', ))
     latitude[:] = numpy.flipud(numpy.arange(-89.75, 90, .5))
     latitude.units = 'degrees_north'
 
-    # Create long dimension & variable:
-    f.createDimension('longitude', 720)
-    longitude = f.createVariable('longitude', 'float', ('longitude',))
+    # Create longitude dimension & variable:
+    ncFile.createDimension('longitude', 720)
+    longitude = ncFile.createVariable('longitude', 'float', ('longitude', ))
     longitude[:] = numpy.arange(-179.75, 180, 0.5)
     longitude.units = 'degrees_east'
+    return
 
-if codebool == 2:
+
+def writeData(ncFile, datafiles):
+    """
+    Name:        writeData
+    Input:       netCDF file, filepath
+    Output:      None.
+    Features:    Reads .asc rasters, skips headers, and adds data to netcdf"""
     # Read .asc files and fill cdf with data
-    evi = f.createVariable('evi', 'f', ('time','latitude','longitude'))
+    evi = ncFile.createVariable('evi', 'f', ('time', 'latitude', 'longitude',))
     evi._FillValue = -.3
     evi.missing_value = -.3
     evi.units = 'unitless'
     evi.valid_min = -0.2
     evi.valid_max = 1
-    for file in datafiles:
-        evi[count] = numpy.genfromtxt(file, skip_header=7, converters = {1: convert})
-    # Reference for Attribute convensions
-    # http://www.bic.mni.mcgill.ca/users/sean/Docs/netcdf/guide.txn_18.html
-    # David Fulker, Unidata Program Center Director, UCAR
+    evi.scale_factor = 10000
+    evicount = 0
+    for file in os.listdir(datafiles):
+        if file.endswith('.txt'):
+            print(file)
+            evi[evicount] = numpy.genfromtxt(file, skip_header=6)
+            evi[evicount] = (evi[evicount]/10000)
+            print(evi[evicount])
+            evicount += 1
+    return
+
+
+###############################################################################
+# MAIN
+###############################################################################
+print('This program will convert zipped .asc files into one netCDF file.')
+print('type "-h" for help')
+zip_folder = input('Enter folder path to the zipped .asc files: ')
+if zip_folder == '-h':
+    print('This is a script for converting .asc raster files',
+          'into the netCDF file format. You can read more about',
+          'the netCDF format at:')
+
+    print('https://www.unidata.ucar.edu/software/',
+          'netcdf/docs/netcdf_introduction.html')
+    zip_folder = input('Enter folder path to the'
+                       'zipped .asc files you wish to convert: ')
+for file in os.listdir(zip_folder):
+    if file.endswith('.zip'):
+        print(file)
+        my_file = os.path.join(zip_folder, file)
+        with ZipFile(my_file, 'r') as zip_ref:
+            zip_ref.extractall(os.path.join(zip_folder))
+my_file = 'test.nc'
+nc_path = os.path.join(zip_folder, my_file)
+f = netcdf.netcdf_file(nc_path, 'w')
+
+writeFileAttributes(f)
+writeDimensions(f, zip_folder)
+writeData(f, zip_folder)
+# Reference for Attribute convensions
+# http://www.bic.mni.mcgill.ca/users/sean/Docs/netcdf/guide.txn_18.html
+# David Fulker, Unidata Program Center Director, UCAR
 
 f.close()
